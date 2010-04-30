c717084dcb9ef5cf99ec9478f3fc32ad24a3fb75
Ryan Booker <ryanbooker@gmail.com>
2010-04-30 16:53:56 +1000

    Fixed spec for allow_destroy

--- spec/unit/mongoid/attributes_spec.rb
+++ spec/unit/mongoid/attributes_spec.rb
@@ -79,7 +79,7 @@ describe Mongoid::Attributes do
 
           it "removes the items that have _destroy => true set" do
             @person.favorites.size.should == 1
-            @person.favorites.first.title == "Ice Cream"
+            @person.favorites.first.title.should == "Jello"
           end
         end
 
